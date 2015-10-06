92291cdfb2c00cd738c8ece699dee5de20c4d0ed
Jamie Eula Evangelista <jevangelista@Jamies-MacBook-Pro-2.local>
2015-10-06 15:57:37 -0700

    adding work for search bar

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,7 +1,15 @@
 class Playlist < ActiveRecord::Base
 
 def self.search(search)
-  where("mood LIKE ?", "%#{search}%") || (where "id LIKE ?", "%#{search}%") 
+
+	if search.to_i > 0
+		where("duration LIKE ?", "%#{search}%") 
+	else
+	  where("mood LIKE ?", "%#{search}%") 
+	end
+
 end
 
 end
+
+
